404b7036e08867a3b565696c0a30c3f6afbc03bd
deeeeeeznutspng <randomdoesstuff@gmail.com>
2025-01-19 15:00:01 -0500

    setup environment variables and basic server, successfully printed and verified api keys

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
+// Load environment variables from the .env file
+require('dotenv').config();
+
+// Access environment variables
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+console.log("Client ID:", clientId); // Test if variables are loaded
+
 const express = require('express');
 const app = express();
 const PORT = 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hello PLAYLIST JUKEBOX WE SO BACKKKKK');    
+    res.send(`client ID: ${clientId}`);
 });
-
+ 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
